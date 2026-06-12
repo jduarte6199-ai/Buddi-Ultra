7f132acc8c6bdb01968152e9d34b919d9d2db039
Grayash <k_2684@naver.com>
2026-06-13 07:53:34 +0900

    chore: bump to v0.1.3

--- buddi/models/Constants.swift
+++ buddi/models/Constants.swift
@@ -72,7 +72,7 @@ extension Defaults.Keys {
     static let menubarIcon = Key<Bool>("menubarIcon", default: true)
     static let showOnAllDisplays = Key<Bool>("showOnAllDisplays", default: false)
     static let automaticallySwitchDisplay = Key<Bool>("automaticallySwitchDisplay", default: true)
-    static let releaseName = Key<String>("releaseName", default: "Legendary Capybara (Spark Propeller)")
+    static let releaseName = Key<String>("releaseName", default: "Common Cactus (Cross None)")
     
     // MARK: Behavior
     static let minimumHoverDuration = Key<TimeInterval>("minimumHoverDuration", default: 0.3)
